6f5fb945f52bd266f14956c63411c44408bd4413
kenna26 <90514808+kenna26@users.noreply.github.com>
2024-01-04 11:24:46 -0500

    csv code

--- AmberMusicCode/AmberMusic/CSVCode.cs
+++ AmberMusicCode/AmberMusic/CSVCode.cs
@@ -1,5 +1,55 @@
+using Csv;
 
 public class CSVCode
 {
-    
+    protected CSV doc;
+    protected List<SongData> allSongData;
+
+    public CSVCode(string path)
+    {
+        doc = new CSV(File.OpenRead("/music.csv"));
+        foreach (var row in doc)
+        {
+            SongData songData = new(row["Song"], row["Artist"], row["Genre"], row["Emotion"]);
+            allSongData.Add(songData);
+        }
+    }
+
+    public override string ToString()
+    {
+        String printing = "";
+        foreach (var item in allSongData)
+        {
+            printing += $"Song: {item.Song}, Artist: {item.Artist}, Genre: {item.Genre}, Emotion: {item.Emotion} \n";
+        }
+
+        return printing;
+    }
+
+    public List<InputData> getInputData()
+    {
+        var inputDatas = new List<InputData>();
+        foreach (var item in allSongData)
+        {
+            inputDatas.Add(new InputData(item.Song, item.Artist, item.Genre));
+        }
+        return inputDatas;
+    }
+
+    public List<string> getOutputData()
+    {
+        var outputDatas = new List<string>();
+        foreach (var item in allSongData)
+        {
+            outputDatas.Add(item.Emotion);
+        }
+
+        return outputDatas;
+
+    }
 }
+
+public record SongData (string Song, string Artist, string Genre, string Emotion);
+//TODO make a ToString for SongData
+
+public record InputData (string Song, string Artist, string Genre);
--- AmberMusicCode/AmberMusic/Program.cs
+++ AmberMusicCode/AmberMusic/Program.cs
@@ -1,34 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Csv;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+NeuralNetwork neuralNetwork = new NeuralNetwork();
+    
+CSVCode musicData = new("/music.csv"); 
+List<InputData> inputData = musicData.getInputData();
+List<string> outputData = musicData.getOutputData();
 
-        //3 Input - 1 Output
-        double[,] training_set_inputs = new double[,] { { 0, 0, 1},
-            { 1, 1, 1},
-            { 1, 0, 1},
-            { 0, 1, 1}};
+Console.WriteLine("Start Training.....");
 
-        double[,] training_set_outputs = new double[,] { {0},
-            {1},
-            {1},
-            {0}};
-
-        NeuralNetwork neuralNetwork = new NeuralNetwork();
-
-
-        Console.WriteLine("Start training ...");
-
-        // Train the neural network using a training set.
-        // Do it 10,000 times
-        neuralNetwork.Train(training_set_inputs, training_set_outputs, 10000);
-        Console.WriteLine("End training ...\n\n");
+// Train the neural network using a training set.
+// Do it 10,000 times
+neuralNetwork.Train(inputData, outputData, 10000);
+Console.WriteLine("End training ...\n\n");
 
         // Predict
         Console.WriteLine("Considering new situation [1, 0, 0] -> ?\n");
